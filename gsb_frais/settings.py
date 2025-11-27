91da170fd9b2183581b5fc6d9f9f6b0fcf81e970
AKHOUN Assad <AKHOUNA@sio.lan>
2025-11-27 17:20:22 +0400

    Config allowed hosts pour prod

--- gsb_frais/settings.py
+++ gsb_frais/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-gsb-frais-2024-change-me-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
